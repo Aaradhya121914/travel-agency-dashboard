0cbbe518407482c12cdeb76f2fdabb2a8113b669
Aaradhya <aaradhybhor.com>
2025-06-17 10:27:48 +0530

    Routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,18 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+// ##What This Means for the App-
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+// When a user visits a path like /dashboard, the app will:
+
+// Render the layout from admin-layout.tsx.
+
+// Inside it, render the dashboard.tsx component.
+
+// This promotes layout-based routing, where multiple pages (like dashboard, settings, etc.) can share a common layout.
+import { type RouteConfig, route, layout } from "@react-router/dev/routes";
+
+export default [
+    layout("routes/admin/admin-layout.tsx",[
+        route('dashboard',"routes/admin/dashboard.tsx"),
+        route('all-users',"routes/admin/all-users.tsx")
+
+    ])
+] satisfies RouteConfig;
